443f5ee9ad07d8f6434a37084492d048143d892f
pat shiu <shiu.patricia@gmail.com>
2022-07-31 19:00:09 -0400

    feat(landing UI): scaffold syllabus preview card

---
+++ www/components/SyllabusCard.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+interface ISyllabusCardProps {}
+
+const SyllabusCard: React.FunctionComponent<ISyllabusCardProps> = (props) => {
+  return (
+    <Card>
+      <Card.Body>
+        <div className="d-flex gap-3">
+          <p className="small">Parson The New School of Design</p>
+          <p className="small">PSAM1028</p>
+          <p className="small">Spring 2019</p>
+        </div>
+        <Card.Title>Web Design Basics</Card.Title>
+        <p className="course-instructors">Pat Shiu</p>
+        <Card.Text className="course-description">
+          Web Design Basics is designed to introduce students to programming as
+          a creative medium—as a way of making and exploring. The coursework
+          focuses on developing a vocabulary of interaction design principles
+          which can then be applied across a range of platforms. Students are
+          encouraged to experiment with various media, tools, and techniques,
+          ultimately producing a portfolio of interactive and visual projects
+          designed for the screen. An emphasis is placed on typography as it
+          applies to a screen context, research-based problem solving and a
+          “learning through making” approach to technical skill building.
+          Historical and current interaction design precedents will be
+          discussed.
+        </Card.Text>
+        <div className="course-tags d-flex gap-2">
+          <Button className="btn-sm btn-tag">web design</Button>
+          <Button className="btn-sm btn-tag">foundation</Button>
+          <Button className="btn-sm btn-tag">undergraduate</Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default SyllabusCard;
