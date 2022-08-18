1b20b3573a97feb458a72312c59f0156781e6532
pat shiu <shiu.patricia@gmail.com>
2022-08-18 16:41:44 -0400

    feat(UI): Add collection card component

---
+++ www/components/CollectionCard.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Link from "next/link";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import SyllabusSchoolCodeYear from "components/Syllabus/SyllabusSchoolCodeYear";
+
+interface ICollectionCardProps {}
+
+const CollectionCard: React.FunctionComponent<ICollectionCardProps> = (
+  props
+) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>
+          <Link href="user/a-collection">
+            <a>Web Design Courses</a>
+          </Link>
+        </Card.Title>
+        <p className="collection-meta small">
+          collection by Pat Shiu | contains 12 syllabi
+        </p>
+        <Card.Text className="course-description">
+          Lorem ipsum a collection about web design, with discussions on
+          historical and current interaction design precedents.
+        </Card.Text>
+        <div className="collection-tags d-flex gap-2">
+          <Button variant="outline-dark" className="btn-sm btn-tag">
+            web design
+          </Button>
+          <Button variant="outline-dark" className="btn-sm btn-tag">
+            foundation
+          </Button>
+          <Button variant="outline-dark" className="btn-sm btn-tag">
+            undergraduate
+          </Button>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CollectionCard;
