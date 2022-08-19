c775551e5f8e53d943f5ad0d5a1064cb48e2e2ac
pat shiu <shiu.patricia@gmail.com>
2022-08-19 02:16:49 -0400

    feat(UI): Scaffold user profile page.

--- www/pages/user/index.tsx
+++ www/pages/user/index.tsx
@@ -3,9 +3,8 @@ import Head from "next/head";
 import Link from "next/link";
 
 import { GlobalNav } from "components/GlobalNav";
-import { FiltersBar } from "components/FiltersBar";
-import SyllabusSchoolCodeYear from "components/Syllabus/SyllabusSchoolCodeYear";
 import CollectionCard from "components/CollectionCard";
+import SyllabusCard from "components/SyllabusCard";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -15,20 +14,18 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
-import SyllabusCard from "components/SyllabusCard";
 
 const About: NextPage = () => {
   return (
     <>
       <Head>
-        <title>About Syllabi Explorer</title>
-        <meta name="description" content="About Syllabi Explorer" />
+        <title>User Name | Syllabi Explorer</title>
+        <meta name="description" content="Syllabi Explorer | user name" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container>
         <div id="header-section" className="sticky-top">
           <GlobalNav />
-          <FiltersBar />
         </div>
         <Row>
           <Col lg="4">
@@ -43,8 +40,8 @@ const About: NextPage = () => {
                   creating and accessing web archives.
                 </p>
                 <p>
-                  Outside of work, I&aposve taught web design at Parsons School of
-                  Design at The New School, and exhibited my art works in
+                  Outside of work, I&apos;ve taught web design at Parsons School
+                  of Design at The New School, and exhibited my art works in
                   digital and physical spaces. I also run a small print studio
                   OfficialFan . Club.
                 </p>
@@ -102,30 +99,77 @@ const About: NextPage = () => {
                 </p>
               </div>
             </div>
+            <div id="user-collection-index" className="py4 mb-5 border-bottom">
+              <h2>Collections</h2>
+              <div id="user-collection-index-public">
+                <h3 className="h6">
+                  Your Collections
+                  <span className="badge bg-success ms-2">Public</span>
+                </h3>
+                <ul className="list-unstyled pb-3">
+                  <li>
+                    <Link href="#">
+                      <a>Web Design History (12)</a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div id="user-syllabi-index-private">
+                <h3 className="h6">
+                  Your Collections
+                  <span className="badge bg-secondary ms-2">Private</span>
+                </h3>
+                <p className="notice-empty text-muted pb-3">
+                  <em>No collections to display.</em>
+                </p>
+              </div>
+            </div>
           </Col>
           <Col>
             <div className="py-4">
               <Tabs
                 defaultActiveKey="syllabi"
-                id="uncontrolled-tab-example"
+                id="user-syllabi-collections-tabs"
                 className="mb-3"
               >
                 <Tab eventKey="syllabi" title="Syllabi">
-                  {/* SYLLABI CONTENT */}
                   <div className="d-flex justify-content-between align-items-baseline py-2">
                     <h2 className="inline h5">Syllabi shared by you</h2>
-                    <Form className="inline col-3">
-                      <Form.Control
-                        type="search"
-                        className="form-control"
-                        placeholder="Search..."
-                        aria-label="Search"
-                      />
-                    </Form>
+                    <div className="d-flex gap-2">
+                      <Form>
+                        <Form.Control
+                          type="Filter"
+                          className="form-control"
+                          placeholder="Filter..."
+                          aria-label="Filter"
+                        />
+                      </Form>
+                      <Link href="/NewSyllabus">
+                        <Button variant="primary" aria-label="New Syllabus">
+                          + New
+                        </Button>
+                      </Link>
+                    </div>
                   </div>
                   <SyllabusCard />
                 </Tab>
                 <Tab eventKey="collections" title="Collections">
+                  <div className="d-flex justify-content-between align-items-baseline py-2">
+                    <h2 className="inline h5">Your Collections</h2>
+                    <div className="d-flex gap-2">
+                      <Form>
+                        <Form.Control
+                          type="Filter"
+                          className="form-control"
+                          placeholder="Filter..."
+                          aria-label="Filter"
+                        />
+                      </Form>
+                      <Button variant="primary" aria-label="New Collection">
+                        + New
+                      </Button>
+                    </div>
+                  </div>
                   <CollectionCard />
                 </Tab>
               </Tabs>
