26e294a7c5fe415e412975d54dc90648d4946d09
pat shiu <shiu.patricia@gmail.com>
2022-08-19 12:30:27 -0400

    feat(UI Syllabus Page): Set up props.

--- www/components/Syllabus/SyllabusFooter.tsx
+++ www/components/Syllabus/SyllabusFooter.tsx
@@ -1,29 +1,34 @@
 import * as React from "react";
+import Link from "next/link";
+
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 interface ISyllabusFooterProps {
-  instructors: string;
   author: string;
+  authorUUID: string;
   uploadDate: string;
 }
 
 const SyllabusFooter: React.FunctionComponent<ISyllabusFooterProps> = ({
-  instructors,
   author,
+  authorUUID,
   uploadDate,
 }) => {
+  const readableDate = new Date(uploadDate).toString();
+  const [weekday, month, day, year, ...theRest] = readableDate.split(" ");
   return (
     <Row className="border-top py-3" id="footer">
-      <Col lg>
-        <p className="small text-center">Syllabus by {instructors}</p>
-      </Col>
-      <Col lg>
+      <Col lg={8}>
         <p className="small text-center">
-          Uploaded by {author} on {uploadDate}
+          Uploaded by{" "}
+          <Link href={`/user/${encodeURIComponent(authorUUID)}`}>
+            <a className="text-muted">{author}</a>
+          </Link>{" "}
+          on {weekday} {month} {day} {year}
         </p>
       </Col>
-      <Col lg>
+      <Col lg={2}>
         <p className="small text-center">flag for review</p>
       </Col>
     </Row>
