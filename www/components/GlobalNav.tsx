dd82183c5cb74f69a1b9be46ebd8372ea09fb956
pat shiu <shiu.patricia@gmail.com>
2022-08-19 12:35:34 -0400

    chore: remove unnecessary imports.

--- www/components/GlobalNav.tsx
+++ www/components/GlobalNav.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
