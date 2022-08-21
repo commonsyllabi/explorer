161bd7af4f0d4e98932ad916519f06d1eb3bb61d
pat shiu <shiu.patricia@gmail.com>
2022-08-21 18:30:46 -0400

    feat(UI): dynamic breadcrumbs bar

--- www/components/BreadcrumbsBar.tsx
+++ www/components/BreadcrumbsBar.tsx
@@ -2,18 +2,29 @@ import Container from "react-bootstrap/Container";
 import Link from "next/link";
 import Dropdown from "react-bootstrap/Dropdown";
 
-import { FunctionComponent } from "react";
-
-export const BreadcrumbsBar: FunctionComponent = () => {
+interface IBreadcrumbsBarProps {
+  user: string;
+  userId: string;
+  category: string;
+  pageTitle: string;
+}
+export const BreadcrumbsBar: React.FunctionComponent<IBreadcrumbsBarProps> = (
+  props
+) => {
   return (
     <Container className="border-bottom">
       <div className="pt-3 d-grid gap-2 d-flex justify-content-between align-items-baseline">
         <div className="breadcrumbs d-flex">
           <Link href="/user">
-            <p>@patshiu&nbsp;</p>
+            <p className="small">
+              <Link href={`/user/${props.userId}`}>
+                <a className="text-muted">@{props.user}</a>
+              </Link>
+              &nbsp;
+            </p>
           </Link>
-          <p>/&nbsp;collections&nbsp;</p>
-          <p>/&nbsp;Web Design History</p>
+          <p className="small">/&nbsp;{props.category}&nbsp;</p>
+          <p className="small">/&nbsp;{props.pageTitle}</p>
         </div>
       </div>
     </Container>
