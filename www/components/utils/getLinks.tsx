a9b507aecda61be4dd8b13a243bde6c2e2d21eab
pat shiu <shiu.patricia@gmail.com>
2022-10-01 20:02:33 -0400

    fix: switch urls from 'collection' to 'collections'

--- www/components/utils/getLinks.tsx
+++ www/components/utils/getLinks.tsx
@@ -3,6 +3,10 @@ export const getSyllabiUrl = (uuid: string) => {
   return "/syllabus/" + uuid;
 };
 
+export const getCollectioniUrl = (uuid: string) => {
+  return "/collections/" + uuid;
+};
+
 export const getUserUrl = (uuid: string) => {
   //-- TODO: switch from user to userS
   return "/user/" + uuid;
