1ab2aa8b69bcd99c36b98188f83dfdc355882110
pat shiu <shiu.patricia@gmail.com>
2022-10-31 13:26:19 -0400

    style: override bs colors

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "node_modules/bootstrap/dist/css/bootstrap.min.css";
 import SSRProvider from "react-bootstrap/SSRProvider";
 import "pages/global_styles.scss";
 import type { AppProps } from "next/app";
@@ -10,12 +10,15 @@ const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL as string;
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID as string;
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-
   return (
     <SessionProvider session={session}>
       <SSRProvider>
         {/* Analytics */}
-        <Script defer data-domain="explorer.common-syllabi.org" src="https://stats.ia-fictions.net/js/plausible.js"></Script>
+        <Script
+          defer
+          data-domain="explorer.common-syllabi.org"
+          src="https://stats.ia-fictions.net/js/plausible.js"
+        ></Script>
         <Component {...pageProps} />
       </SSRProvider>
     </SessionProvider>
