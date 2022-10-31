e05e7ce654ad20acce4346b7e46b759d2c2ca9bb
période <pierre.depaz@gmail.com>
2022-10-31 20:01:12 +0100

    added tagline

--- www/components/GlobalNav.tsx
+++ www/components/GlobalNav.tsx
@@ -18,19 +18,22 @@ const GlobalNav: React.FunctionComponent = () => {
         expand="lg"
         className="d-flex justify-content-md-between border-bottom"
       >
-        <Container>
+        <Container className="flex">
           <Navbar.Brand href="/">Cosyll</Navbar.Brand>
+          <Navbar.Text>Browse and publish syllabi.</Navbar.Text>
           <Navbar.Toggle aria-controls="global-nav" />
         </Container>
         <Container>
           <Navbar.Collapse id="global-nav" className="flex-row-reverse">
             <Nav className="float-end">
+
+
               <Nav.Link
-                href="/NewSyllabus"
+                href="/about"
                 className="py-3 text-end"
-                data-cy="newSyllabusLink"
+                data-cy="aboutlink"
               >
-                + New Syllabus
+                About
               </Nav.Link>
 
               <NavDropdown
@@ -47,10 +50,6 @@ const GlobalNav: React.FunctionComponent = () => {
                   My Account
                 </NavDropdown.Item>
 
-                <NavDropdown.Item href="/about" className="py-2 text-end">
-                  About
-                </NavDropdown.Item>
-
                 <NavDropdown.Divider />
 
                 <NavDropdown.Item
@@ -65,6 +64,14 @@ const GlobalNav: React.FunctionComponent = () => {
                   Sign Out
                 </NavDropdown.Item>
               </NavDropdown>
+
+              <Nav.Link
+                href="/NewSyllabus"
+                className="py-3 text-end"
+                data-cy="newSyllabusLink"
+              >
+                + New Syllabus
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -79,6 +86,7 @@ const GlobalNav: React.FunctionComponent = () => {
       >
         <Container>
           <Navbar.Brand href="/">Cosyll</Navbar.Brand>
+          <Navbar.Text>Browse and publish syllabi.</Navbar.Text>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
         </Container>
         <Container>
